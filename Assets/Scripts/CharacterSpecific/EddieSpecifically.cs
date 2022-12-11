e3e4a1bc2e2f57b31a0a758d1606c864e45df224
Lisa Olsson <97893739+LisaOlsson2@users.noreply.github.com>
2022-12-11 14:34:56 +0100

    cheese

--- Assets/Scripts/CharacterSpecific/EddieSpecifically.cs
+++ Assets/Scripts/CharacterSpecific/EddieSpecifically.cs
@@ -6,12 +6,4 @@ using UnityEngine.UI;
 public class EddieSpecifically : Characters
 {
 
-
-    public override void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-
-        }
-    }
 }
